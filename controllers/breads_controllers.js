df4cdda8769c0f6eda7cb59f335546438d915f33
BuildTools <unconfigured@null.spigotmc.org>
2022-12-03 12:38:20 -0700

    Implemented Mongoose ODM

--- controllers/breads_controllers.js
+++ controllers/breads_controllers.js
@@ -4,12 +4,19 @@ const Bread = require('../models/bread.js')
 
 //INDEX
 breads.get('/', (req, res) => {
-    res.render('Index',
-        {
-            breads: Bread,
-            title: 'Index Page'
-        }
-    )
+  Bread.find()
+    .then(foundBreads => {
+      res.render('index', {
+        breads: foundBreads,
+        title: 'Index Page'
+      })
+    })  
+  // res.render('Index',
+    //     {
+    //         breads: Bread,
+    //         title: 'Index Page'
+    //     }
+    // )
     // res.send(Bread)
 })
 
@@ -25,30 +32,33 @@ breads.get('/new', (req, res) => {
 })
 
 // SHOW
-breads.get('/:arrayIndex', (req, res) => {
-    if (Bread[req.params.arrayIndex]) {
-      res.render('Show', {
-        bread:Bread[req.params.arrayIndex],
-        index: req.params.arrayIndex,
+breads.get('/:id', (req, res) => {
+  Bread.findById(req.params.id)
+      .then(foundBread => {
+          res.render('show', {
+              bread: foundBread
+          })
       })
-    } else {
-      res.render('404')
-    }
-  })  
+      .catch(err => {
+        res.send('404')
+      })
+})
+
 
 // CREATE
 breads.post('/', (req, res) => {
-    if (!req.body.image) {
-      req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
-    }
-    if(req.body.hasGluten === 'on') {
-      req.body.hasGluten = true
-    } else {
-      req.body.hasGluten = false
-    }
-    Bread.push(req.body)
-    res.redirect('/breads')
-  })
+  if(!req.body.image) {
+      req.body.image = undefined 
+  }
+  if(req.body.hasGluten === 'on') {
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread.create(req.body)
+  res.redirect('/breads')
+})
+
   
 // UPDATE
 breads.put('/:arrayIndex', (req, res) => {
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //DEPENDENCIES
 const express = require('express')
 const methodOverride = require('method-override')
+const mongoose = require('mongoose')
 
 //CONFIGURATION
 require('dotenv').config()
@@ -14,7 +15,10 @@ app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
-
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+  )
+  
 
 //ROUTES
 app.get('/', (req, res) => {
